ad05c066a3126a36c2e2807639e87e46132ec339
Oriana <oriana.ramos@vyrgs.com>
2023-03-29 18:09:23 -0500

    Mofificacion el la ruta logina

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,118 +1,131 @@
-import * as React from 'react';
-import { useState } from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { useNavigate } from 'react-router-dom';
-import { FormControl, FormHelperText, FormLabel, Input } from '@mui/material';
-import { useForm } from 'react-hook-form';
-import { baseUrl } from '../../helpers/config';
-import axios from 'axios';
-import { saveLogin } from '../../helpers/localStorage';
+import * as React from "react";
+import { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useNavigate } from "react-router-dom";
+import { FormControl, FormHelperText, FormLabel, Input } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { baseUrl } from "../../helpers/config";
+import axios from "axios";
+import { saveLogin } from "../../helpers/localStorage";
 
 export default function Login() {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const navigate = useNavigate();
-  
-  const{handleSubmit,register, formState:{errors}}=useForm();
-   
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
   const onSubmit = (data) => {
     setIsSubmitting(true);
     console.log(data);
     axios
       .post(`${baseUrl()}/users/login/`, data)
       .then((response) => {
-         console.log("response", response.data.data);
-        saveLogin(response.data.data); 
+        console.log("response", response.data.data);
+        saveLogin(response.data.data);
         navigate("/new-reports");
       })
       .catch((data) => {
-        console.log("error",data)
+        console.log("error", data);
         setIsSubmitting(false);
       });
   };
 
   return (
-   <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <FormControl id="name" >
-              <FormLabel>Usuario</FormLabel>
-              <Input
-                type="text"
-                name="username"
-                {...register("username", {
-                  required: "Usuario requerido",
-                  minLength: { value: 4, message: "El usuario debe tener 4 caracteres como minimo" },
-                })}
-              />
-            </FormControl>
-            <FormHelperText> {errors.username && errors.username.message}</FormHelperText>
-           
-            <FormControl id="password" >
-              <FormLabel>Clave</FormLabel>
-              <Input
-                type="password"
-                name="password"
-                {...register("password", {
-                  required: "Clave requerida",
-                  minLength: { value: 4, message: "La clave debe tener 4 caracteres como minimo" },
-                })}
-              />
-            </FormControl>
-             <FormHelperText>{errors.password && errors.password.message} </FormHelperText>
-
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Recordar"
+          <FormControl id="name">
+            <FormLabel>Usuario</FormLabel>
+            <Input
+              type="text"
+              name="username"
+              {...register("username", {
+                required: "Usuario requerido",
+                minLength: {
+                  value: 4,
+                  message: "El usuario debe tener 4 caracteres como minimo",
+                },
+              })}
             />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2}}
-              loading={isSubmitting}
-            >
-              Ingresar
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  olvidaste tu contraseña?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"No tienes cuenta?  Registrate"}
-                </Link>
-              </Grid>
+          </FormControl>
+          <FormHelperText>
+            {" "}
+            {errors.username && errors.username.message}
+          </FormHelperText>
+
+          <FormControl id="password">
+            <FormLabel>Clave</FormLabel>
+            <Input
+              type="password"
+              name="password"
+              {...register("password", {
+                required: "Clave requerida",
+                minLength: {
+                  value: 4,
+                  message: "La clave debe tener 4 caracteres como minimo",
+                },
+              })}
+            />
+          </FormControl>
+          <FormHelperText>
+            {errors.password && errors.password.message}{" "}
+          </FormHelperText>
+
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Recordar"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            loading={isSubmitting}
+          >
+            Ingresar
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                olvidaste tu contraseña?
+              </Link>
             </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"No tienes cuenta?  Registrate"}
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
       </Container>
-   </form>
-    
-   
+    </form>
   );
 }
--- src/routers/RouterApp.jsx
+++ src/routers/RouterApp.jsx
@@ -1,32 +1,34 @@
-
-import React from 'react'
-import {Route,Routes } from 'react-router-dom'
-import MenuLista from '../components/Menu/MenuLista'
-import Error404 from '../error/Error404'
-import GenerarReporte from '../pages/GenerarReporte'
-import Login from '../pages/login/Login'
-import NewReports from '../pages/NewReports'
-import ReportLayout from '../pages/ReportLayout'
-import ReportColums from '../pages/ReportsColums'
-import Welcom from '../pages/Welcom'
-import LoginAs from './loginas'
-import Root from './Root'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import MenuLista from "../components/Menu/MenuLista";
+import Error404 from "../error/Error404";
+import GenerarReporte from "../pages/GenerarReporte";
+import Login from "../pages/login/Login";
+import NewReports from "../pages/NewReports";
+import ReportLayout from "../pages/ReportLayout";
+import ReportColums from "../pages/ReportsColums";
+import Welcom from "../pages/Welcom";
+import LoginAs from "./loginas";
+import Root from "./Root";
 
 const RouterApp = () => {
   return (
-   <Routes>
-    <Route path="*" element={<h1>No encontrado... (404)</h1>}></Route>
-    <Route path="/" element={<Root />}></Route>
-    <Route path="/login" element={<Login />}></Route>
-    <Route path='/new-reports' element={<MenuLista/>}></Route>
-    <Route path='/Welcome' element={<Welcom/>}></Route>
-    <Route path="/loginas/:token" element={<LoginAs/>}></Route>
-    <Route path='/new-reports/NewReports' element={<NewReports/>}></Route>
-    <Route path='/new-reports/ReportLayout' element={<ReportLayout/>}></Route>
-    
-    <Route path='Error404' element={<Error404/>} ></Route>
-   </Routes>
-  )
-}
+    <Routes>
+      <Route path="*" element={<h1>No encontrado... (404)</h1>}></Route>
+      <Route path="/" element={<Root />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/loginas/:token" element={<LoginAs />}></Route>
+      <Route path="/new-reports" element={<MenuLista />}></Route>
+      
+      <Route path="/Welcome" element={<Welcom />}></Route>
+      <Route path="/new-reports/NewReports" element={<NewReports />}></Route>
+      <Route
+        path="/new-reports/ReportLayout"
+        element={<ReportLayout />}
+      ></Route>
+      <Route path="Error404" element={<Error404 />}></Route>
+    </Routes>
+  );
+};
 
-export default RouterApp
+export default RouterApp;
